963016b261cacc6988e0db1fdbde71e76c3e249c
GEANICEV <geanicev@gmail.com>
2026-04-27 22:52:57 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,39 +1,24 @@
-const CACHE_NAME = 'calsindo-pwa-v1';
+const CACHE_NAME = 'calsindo-pwa-v2';
+
 const urlsToCache = [
   './',
   './index.html',
+  './manifest.json',
   './calsindo.jpeg',
   'https://cdn.tailwindcss.com',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css'
 ];
 
-self.addEventListener('install', (event) => {
+self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(urlsToCache))
+      .then(cache => cache.addAll(urlsToCache))
   );
-  self.skipWaiting();
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then((response) => {
-        return response || fetch(event.request);
-      })
-  );
-});
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
+      .then(response => response || fetch(event.request))
   );
 });
